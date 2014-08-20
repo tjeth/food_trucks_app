b0d9b2d1f75f9c87c20dc40f5159e53092488c45
Tim Wu <timwu@twu.(none)>
2014-08-19 22:39:05 -0700

    Changed directory structure

---
+++ food_trucks_app.py
@@ -0,0 +1,195 @@
+from flask import Flask, render_template, send_file, request, jsonify
+import requests
+import threading
+import json
+import time
+from werkzeug.contrib.cache import SimpleCache
+from math import radians, cos, sin, asin, sqrt
+
+app = Flask(__name__)
+
+# Google Maps API
+API_KEY = 'AIzaSyCFPJGIIcbIUmbIitqx2chf5pqdYHGTqsI'
+# Key: item address, Value: location item
+no_latlong_cache = SimpleCache() 
+# Background thread for refreshing locations
+refresh_thread = threading.Thread()
+
+def refresh_locations():
+  print "Refreshing cache"
+
+  # Run once per day
+  t = threading.Timer(86400.0, refresh_locations)
+  # = threading.Timer(10.0, refresh_locations)
+  
+  # Allows you to interrupt program cleanly
+  t.daemon = True      
+  t.start()
+
+  # Request to food trucks data API
+  response = requests.get('http://data.sfgov.org/resource/rqzj-sfat.json').content
+
+  data = json.loads(response)
+
+  locations = []
+  no_latlong = []
+
+  build_locations_lists(data, locations, no_latlong)
+  geocode_no_latlong_locations(locations, no_latlong)
+
+  f = open('food_truck_data.json','w')
+  json.dump(locations, f, indent=0)
+  f.close()
+
+
+# Build locations list with relevant data for views
+# Add to a list for geocoding if does not have latlong
+def build_locations_lists(data, locations, no_latlong):
+  id_count = 1
+
+  for x in range(0, len(data)):
+    item = data[x]
+    if (item['status'] == 'APPROVED'):
+      if ('location' not in item and 'address' in item):
+        item['id'] = id_count
+        no_latlong.append(item)
+      elif ('location' in item and 'address' in item): 
+        # Found with location and address
+        locations.append({ 'lat' : item['location']['latitude'], \
+            'lng' : item['location']['longitude'], \
+            'applicant' : item['applicant'], \
+            'address' : item['address'], \
+            'fooditems' : item['fooditems'],
+            'id' : id_count })
+      elif ('location' in item and 'address' not in item): 
+        # Has latlong but no address
+        locations.append({ 'lat' : item['location']['latitude'], \
+            'lng' : item['location']['longitude'], \
+            'applicant' : item['applicant'], \
+            'address' : 'No Address', \
+            'fooditems' : item['fooditems'],
+            'id' : id_count })
+      else:
+        print 'Truck does not have latlong or address!\n{}'.format(str(item))
+      id_count += 1
+
+  print len(locations)
+  print len(no_latlong)
+
+
+# Geocode the locations with no latlongs using Google Maps API
+# Use cache to speed things up on subsequent iterations
+# Finally, add item to locations list
+def geocode_no_latlong_locations(locations, no_latlong):
+  for x in range(0, len(no_latlong)):
+    item = no_latlong[x]
+    cache_key = item['address']
+    cache_item = no_latlong_cache.get(cache_key)
+
+    if (cache_item):
+      locations.append(cache_item)
+    else:
+      addr = item['address'] + ', San Francisco'
+      payload = {'address': addr, 'key': API_KEY}
+      resp = requests.get('https://maps.googleapis.com/maps/api/geocode/json', params=payload).content
+      resp_data = json.loads(resp)
+
+      geo_loc = resp_data['results'][0]['geometry']['location']
+      location_item = { 'lat' : geo_loc['lat'], \
+          'lng' : geo_loc['lng'],\
+          'applicant' : item['applicant'],\
+          'address' : item['address'],\
+          'fooditems' : item['fooditems'],
+          'id' : item['id']}
+      locations.append(location_item)
+      no_latlong_cache.set(cache_key, location_item, timeout=30*24*60) #30 day timeout
+
+  print len(locations) 
+
+
+def execute_refresh_locations():
+  global refresh_thread
+  # Start the timer loop that runs every 24 hours
+  refresh_thread = threading.Thread(target=refresh_locations).start()
+
+
+# Execute refreshing locations in a separate thread 
+execute_refresh_locations()
+
+
+# Haversine formula for calculating distance b/w two points on sphere
+# Source: http://stackoverflow.com/a/4913653
+def haversine(lon1, lat1, lon2, lat2):
+  """
+  Calculate the great circle distance between two points 
+  on the earth (specified in decimal degrees)
+  """
+  # convert decimal degrees to radians 
+  lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+
+  # haversine formula 
+  dlon = lon2 - lon1 
+  dlat = lat2 - lat1 
+  a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+  c = 2 * asin(sqrt(a)) 
+
+  # 6367 km is the radius of the Earth
+  km = 6367 * c
+  return km 
+
+
+@app.route("/")
+def index():
+  return render_template('index.html')
+
+
+@app.route("/food_truck_locations")
+def food_truck_locations():
+  return send_file('food_truck_data.json')
+
+
+@app.route("/address_search")
+def address_search():
+  # TODO: Turn this into modular code (same code as above)
+  addr = request.args.get("address")
+  payload = {'address': addr, 'key': API_KEY}
+  resp = requests.get('https://maps.googleapis.com/maps/api/geocode/json', params=payload).content
+  resp_data = json.loads(resp)
+
+  if len(resp_data['results']) != 0:
+
+    print resp_data['results']
+
+    formatted_address = resp_data['results'][0]["formatted_address"]
+
+    # Make sure address is in San Francisco
+    if "San Francisco" in formatted_address:
+      geo_loc = resp_data['results'][0]['geometry']['location']
+
+      lat1 = float(geo_loc['lat'])
+      lng1 = float(geo_loc['lng'])
+
+      search_latlong = {'lat': lat1, 'lng': lng1}
+
+      within_mile = []
+      json_data = open('food_truck_data.json')
+      data = json.load(json_data)
+      for location in data:
+        lat2 = float(location['lat'])
+        lng2 = float(location['lng'])
+        haversine_distance = haversine(lng1, lat1, lng2, lat2)
+
+        if (haversine_distance < 1.0): # Less than one kilometer away
+          within_mile.append(location)
+
+      json_data.close()
+
+      print len(within_mile)
+      return jsonify(search_address=search_latlong, results=within_mile)
+    else:
+      return "Please input a San Francisco address!"
+
+  return "No address search results"
+
+if __name__ == "__main__":
+  app.run(debug=True)
---
+++ static/food_trucks.js
@@ -0,0 +1,266 @@
+$(document).ready(function() {
+  console.log("Inside beginning of food_truck.js");
+
+  /* Google Maps related code */
+
+  // Initialize the map
+  var map = new google.maps.Map($('#map-canvas')[0], {
+    center: new google.maps.LatLng(37.7833, -122.4167),
+    zoom: 12,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  });
+
+
+  // Handle to the Google Maps marker collection
+  var markerCollectionView;
+
+
+  // Food Truck Location Collection 
+  // Populated by data from the server
+  var TruckLocationsList = Backbone.Collection.extend({
+    // Reference to this collection's model.
+    model: Backbone.GoogleMaps.Location,
+
+    url: '/food_truck_locations',
+
+    parse: function(response) {
+      console.log("Parsing response from server");
+      return parseDataForMap(response);
+    }
+
+  });
+
+
+  // Create our global collection of TruckLocations
+  var TruckLocations = new TruckLocationsList();
+
+
+  // Fetch location data from the server
+  TruckLocations.fetch({
+    success: function(response, xhr) {
+      console.log("Inside TruckLocations.fetch success callback");
+      drawPins(response.models);
+    },
+    error: function (errorResponse) {
+      console.log(errorResponse);
+    }
+  });
+
+
+
+  /* Address search related code */
+
+  // View for searching trucks near the specified address
+  AddressSearchView = Backbone.View.extend({
+    initialize: function(){
+      this.render();
+    },
+
+    render: function(){
+      var template = _.template( $("#address_search_template").html(), {} );
+      this.$el.html( template );
+    },
+
+    events: {
+      "click input[type=button]": "performSearch"
+    },
+
+    // Performs search by making a request to the server and then parsing data
+    performSearch: function( event ){
+      console.log( "Search for " + $("#address_search_input").val() );
+
+      var address = $("#address_search_input").val();
+      $.get( "/address_search", { 'address': address }, function( data ) {
+        console.log("Inside success of address_search");
+
+        // Parse response from server if returned data
+        if (jQuery.type(data) !== 'string') {
+          var locations = data['results'];
+          var truck_list = parseDataForMap(locations);
+
+          var table_truck_list = parseDataForTable(locations);
+
+          var pt = new google.maps.LatLng(data['search_address']['lat'], 
+            data['search_address']['lng']);
+          map.setCenter(pt);
+          map.setZoom(15);
+
+          drawPins(truck_list);
+
+          drawGrid(table_truck_list);
+        } else {
+          // Alert the error message
+          alert(data);
+        }
+      })
+      .fail(function() {
+        console.log( "GET request to address_search failed" );
+      });
+    }
+  });
+
+
+  // Create the address search view
+  var address_search_view = new AddressSearchView({ 
+    el: $("#address_search_container") 
+  });
+
+
+  // Helper function for drawing pins on the map
+  function drawPins(data) {
+    var places = new Backbone.GoogleMaps.LocationCollection(data);
+
+    // Delete markers if there were previous markers on the map
+    if (markerCollectionView) {
+      markerCollectionView.closeChildren();
+    }
+
+    // Render Markers
+    markerCollectionView = new Backbone.GoogleMaps.MarkerCollectionView({
+      collection: places,
+      map: map
+    });
+    markerCollectionView.render();
+  }
+
+
+
+
+  /* Data Table (backgrid.js) related code */
+
+  // Handle to grid object
+  var grid; 
+
+
+  // Food Truck Location Model (model for Backgrid table)
+  var TruckLocationTable = Backbone.Model.extend({
+    defaults: function() {
+      return {
+        id: "Id",
+        truck_name: "Truck name",
+        address: "Street address",
+        food_items: "Food items"
+      };
+    }
+  });
+
+
+  // Truck Location Collection (for Backgrid table)
+  var TruckLocationsListTable = Backbone.Collection.extend({
+    model: TruckLocationTable,
+
+    url: "/food_truck_locations",
+
+    parse: function(response) {
+      console.log("Parsing response from server");
+      var table_truck_list = parseDataForTable(response);
+      return table_truck_list;
+    }
+
+  });
+
+
+  // Create Collection
+  var TruckLocationsTable = new TruckLocationsListTable();
+
+
+  // Create columns for Backgrid table
+  var columns = [{
+      name: "id", // The key of the model attribute
+      label: "ID", 
+      editable: false, 
+      cell: Backgrid.IntegerCell.extend({
+        orderSeparator: ''
+      }),
+      direction: "descending"
+    }, {
+      name: "truck_name",
+      label: "Truck Name",
+      cell: "string", 
+      editable: false
+    }, {
+      name: "address",
+      label: "Address",
+      cell: "string", 
+      editable: false
+    }, {
+      name: "food_items",
+      label: "Food Items",
+      cell: "string", 
+      editable: false
+  }];
+
+
+  // Fetch some countries from the url
+  TruckLocationsTable.fetch({
+    reset: true,
+    success: function(response, xhr) {
+      console.log("Inside TruckLocationsTable.fetch success callback");
+      drawGrid(response.models);
+    },
+    error: function (errorResponse) {
+      console.log(errorResponse);
+    }
+  });
+
+
+  // Helper function to draw the backgrid grid 
+  function drawGrid(data) {
+    var table_locations = new TruckLocationsListTable(data);
+
+    if (grid) {
+      grid.remove();
+    }
+
+    grid = new Backgrid.Grid({
+      columns: columns,
+      collection: table_locations
+    });
+
+    // Render the grid and attach the root to your HTML document
+    $("#backgrid_table").append(grid.render().el);
+  }
+
+
+
+
+  /* Data parsing helper functions */
+
+  // Helper function for parsing data for use with Google Maps markers
+  function parseDataForMap(response) {
+    var truck_list = [];
+
+    var i;
+    for (i = 0; i < response.length; i++) {
+      var truck_object = {};
+      truck_object["title"] = response[i]["applicant"] + " (id: " +
+        response[i]["id"] + ")";
+      truck_object["lat"] = response[i]["lat"];
+      truck_object["lng"] = response[i]["lng"];
+      truck_list.push(truck_object);
+    }
+
+    return truck_list;
+  }
+
+
+  // Helper function for parsing data for use backgrid data table
+  function parseDataForTable(response) {
+    var table_truck_list = [];
+
+    var i;
+    for (i = 0; i < response.length; i++) {
+      var table_truck_object = {};
+      table_truck_object["id"] = response[i]["id"];
+      table_truck_object["truck_name"] = response[i]["applicant"];
+      table_truck_object["address"] = response[i]["address"];
+      table_truck_object["food_items"] = response[i]["fooditems"];
+      table_truck_list.push(table_truck_object);
+    }
+
+    // Sort items by id ascending
+    table_truck_list.sort(function(a, b){return a['id'] - b['id']});
+
+    return table_truck_list;
+  }
+});
